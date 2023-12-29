3b39d613bdb81e8a344b778fcae0cb11f31d18c8
kinoman420 <armangholizadeh48@gmail.com>
2023-12-29 18:59:40 +0330

    make ctrl+ alphabet keys valid

--- kino.c
+++ kino.c
@@ -6,6 +6,13 @@
 #include <termios.h>
 #include <unistd.h>
 
+/***defines***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
+
+
 /*** data ***/
 struct termios orig_termios;
 
@@ -71,7 +78,7 @@ int main() {
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q') break;
+        if (c == CTRL_KEY('q')) break;
     }
 
 
