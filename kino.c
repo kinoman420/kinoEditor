849e84d63a504a23214b37e483d92547727b2cc1
kinoman420 <armangholizadeh48@gmail.com>
2023-12-29 21:19:49 +0330

    added buffer,printing ~,window size, etc...

--- kino.c
+++ kino.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -14,28 +16,41 @@
 
 
 /*** data ***/
-struct termios orig_termios;
+
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
+
 void die(const char *s) {
+
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
 
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode() {
 
     //Get Terminal State
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcgetattr");
 
     atexit(disableRawMode);
     
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     //Turn off Output processing ( carriage return and newline \r\n )
     //since this disable carriage return, we shall add it manually to printf
@@ -61,25 +76,153 @@ void enableRawMode() {
 }
 
 
+//simplified main, this function waits for keypress and returns it
+char editorReadKey() {
+    int nread;
+    char c;
+    while (( nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+
+int getCursorPosition(int *rows, int *cols) {
+
+    char buf[32];
+    unsigned int i = 0;
+
+    // query terminal status using n command...
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+        
+
+    while (i < sizeof(buf) - 1) {
+        if( read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i == 'R']) break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if( buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if( sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+// get window size using ioctl(), uses a fallback method if it fails ( we move the cursor to bottom right)
+// then use escape sequences to get the number of rows and cols
+// we use C and B command with 999 argument, C moves the cursor to right and B moves it downward
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0 ) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)return -1;
+        return getCursorPosition(rows, cols);
+            
+        
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/*** append buffer ***/
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+//append string to buffer
+void abAppend(struct abuf *ab, const char *s, int len) {
+    // use realloce to get a block of memory with size of current str + str
+    char *new = realloc(ab->b,ab->len + len);
+
+    if ( new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len =+len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+
+/*** output ***/
+
+
+void editorDrawRows(struct abuf *ab) {
+    int y;
+    for ( y=0; y < E.screenrows; y++) {
+        abAppend(ab,"~",1);
+
+        if ( y < E.screenrows - 1) {
+            abAppend(ab,"\r\n", 2);
+        }
+    }
+}
+
+void editorRefreshScreen(){
+
+    struct abuf ab = ABUF_INIT;
+
+    // we write escape character using this. the J command clears the screen and the argument is 2
+    // H command is cursor reposition
+
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+
+
+
+/*** input ***/
+
+
+//handles the input from editorReadkey
+void editorProcessKeyPress() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+        exit(0);
+        break;
+    }
+}
+
+
 /*** init ***/
+
+
+// initializes row and col sizes
+
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)die("getWindowSize");
+}
 
 int main() {
     enableRawMode();
-    while (1) {
-        char c = '\0';
+    initEditor();
 
-        //cygwin return -1 with and errno of EAGAIN when read() times out
-        //so we ignore it
-
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)die("read");
-
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q')) break;
+    while(1) {
+        editorRefreshScreen();
+        editorProcessKeyPress();
     }
+
 
 
     return 0;
