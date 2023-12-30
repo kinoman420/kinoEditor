2b3858ba0b4b1825d765d2310f4a4db761fa46d7
kinoman420 <armangholizadeh48@gmail.com>
2023-12-30 20:56:14 +0330

    read one line

--- kino.c
+++ kino.c
@@ -1,9 +1,17 @@
 /*** includes ***/
+
+// multiplatform support
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -31,10 +39,21 @@ enum editorKey {
 
 /*** data ***/
 
+
+typedef struct erow {
+    // stores a line of text as pointer
+
+    int size;
+    char *chars;
+    
+}erow;
+
 struct editorConfig {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow row;
     struct termios orig_termios;
 
 };
@@ -193,6 +212,34 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+
+/*** file i/o ***/
+
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die("fopen");
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != -1) {
+        while (linelen > 0 && (line[linelen -1] == '\n' ||
+                               line[linelen -1] == '\r' ))
+            linelen--;
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+    free(line);
+    fclose(fp);
+
+    
+}
+
+
 /*** append buffer ***/
 struct abuf {
     char *b;
@@ -223,20 +270,29 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for ( y = 0; y < E.screenrows; y++) {
-        if ( y == E.screenrows / 3) {
-            char welcome[80];
-            int welcomelen = snprintf(welcome, sizeof(welcome), "Kino editor -- version %s", KINO_VERSION);
-            if(welcomelen > E.screencols) welcomelen = E.screencols;
-            int padding = (E.screencols - welcomelen) / 2;
-            if (padding) {
-                abAppend(ab, "~", 1);
-                padding--;
-            }
+        if ( y >= E.numrows) {
+
+            if (E.numrows == 0 && y == E.screenrows / 3) {
+                char welcome[80];
+                int welcomelen = snprintf(welcome, sizeof(welcome), "Kino editor -- version %s", KINO_VERSION);
+                if(welcomelen > E.screencols) welcomelen = E.screencols;
+                int padding = (E.screencols - welcomelen) / 2;
+                if (padding) {
+                    abAppend(ab, "~", 1);
+                    padding--;
+                }
             while (padding--) abAppend(ab, " ", 1);
             abAppend(ab, welcome, welcomelen);
+            } else {
+                abAppend(ab, "~", 1);
+            }
+
         } else {
-            abAppend(ab, "~", 1);
+            int len = E.row.size;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, E.row.chars, len);
         }
+        
 
         abAppend(ab, "\x1b[K", 3);
         if ( y < E.screenrows - 1) {
@@ -353,12 +409,17 @@ void initEditor() {
 
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
+    if (argc >= 2) {
+        editorOpen(argv[1]);
+    }
 
     while(1) {
         editorRefreshScreen();
