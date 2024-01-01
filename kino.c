28f363d3f4ed8752e8e8ee6312ad223204d19682
kinoman <101034920+kinoman420@users.noreply.github.com>
2024-01-01 20:38:28 +0330

    delete message etc..

--- kino.c
+++ kino.c
@@ -24,6 +24,12 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KINO_VERSION "0.0.1"
 #define KINO_TAB_STOP 8
+#define KINO_QUIT_TIMES 3
+
+
+
+
+
 enum editorKey {
 
     // backspace doesn't have a human readable escape sequence representation 
@@ -66,6 +72,7 @@ struct editorConfig {
     int screencols;
     int numrows;
     erow *row;
+    int dirty;
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
@@ -282,6 +289,7 @@ void editorAppendRow(char *s, size_t len) {
     E.row[at].rsize = 0;
     E.row[at].render = NULL;
     E.numrows ++;
+    E.dirty++;
 }
 
 // inserts character in defined row
@@ -293,6 +301,17 @@ void editorRowInsertChar(erow *row, int at, int c) {
     row->size++;
     row->chars[at] = c;
     editorUpdateRow(row);
+    E.dirty++;
+}
+
+// for deleting character: memmove the rest of row to left
+
+void editorRowDelChar(erow *row, int at) {
+  if (at < 0 || at >= row->size) return;
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
 }
 
 
@@ -306,6 +325,19 @@ void editorInsertChar(int c) {
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+
+
+// if we are at end of file, return. else get the character to the left of cursor,delete, and move to left
+void editorDelChar() {
+  if (E.cy == E.numrows) return;
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  }
 }
 
  
@@ -356,6 +388,7 @@ void editorOpen(char *filename) {
   }
     free(line);
     fclose(fp);
+    E.dirty = 0;
 
     
 }
@@ -374,6 +407,7 @@ void editorSave() {
             if (write(fd, buf, len) == len) {
                 close(fd);
                 free(buf);
+                E.dirty = 0;
                 editorSetStatusMessage("%d bytes written to disk", len);
                 return;
             }
@@ -484,7 +518,8 @@ void editorDrawStatusBar(struct abuf *ab) {
 
     // make status short in case it doesn't fit
     char status[80], rstatus[80];
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",E.filename ? E.filename : "[No Name]", E.numrows,
+    E.dirty ? "(modified)" : "");
     
     int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
     if (len > E.screencols) len = E.screencols;
@@ -599,6 +634,10 @@ void editorMoveCursor(int key) {
 
 //handles the input from editorReadkey
 void editorProcessKeyPress() {
+
+    static int quit_times = KINO_QUIT_TIMES;
+
+
     int c = editorReadKey();
 
     switch (c) {
@@ -609,6 +648,13 @@ void editorProcessKeyPress() {
 
         
         case CTRL_KEY('q'):
+
+            if (E.dirty && quit_times > 0) {
+                editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+                "Press Ctrl-Q %d more times to quit.", quit_times);
+                quit_times--;
+                return;
+            }
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
@@ -631,7 +677,8 @@ void editorProcessKeyPress() {
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
-
+            if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+            editorDelChar();
             break;
 
 
@@ -669,6 +716,8 @@ void editorProcessKeyPress() {
             editorInsertChar(c);
             break;
     }
+
+    quit_times = KINO_QUIT_TIMES;
 }
 
 
@@ -686,6 +735,7 @@ void initEditor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.dirty = 0;
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
