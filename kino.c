4a92ac66bfadc69258cda0ef54ffa3d737b992ce
kinoman <101034920+kinoman420@users.noreply.github.com>
2024-01-01 22:15:18 +0330

    basic functions finished

--- kino.c
+++ kino.c
@@ -84,6 +84,10 @@ struct editorConfig E;
 
 /*** prototypes ***/
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
+
+
 
 
 /*** terminal ***/
@@ -446,7 +450,13 @@ void editorOpen(char *filename) {
 }
 
 void editorSave() {
-    if (E.filename == NULL) return;
+    if (E.filename == NULL) {
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL) {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
+  }
 
     int len;
     char *buf = editorRowsToString(&len);
@@ -643,6 +653,44 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /*** input ***/
 
+
+
+// ask user for filename
+
+char *editorPrompt(char *prompt) {
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+  size_t buflen = 0;
+  buf[0] = '\0';
+  while (1) {
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+
+    int c = editorReadKey();
+    if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
+        if (buflen != 0) buf[--buflen] = '\0';
+    }
+    else if (c == '\x1b') {
+        editorSetStatusMessage("");
+        free(buf);
+        return NULL;
+    }
+    else if (c == '\r') {
+      if (buflen != 0) {
+        editorSetStatusMessage("");
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) {
+      if (buflen == bufsize - 1) {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
+
 void editorMoveCursor(int key) {
     erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
@@ -714,7 +762,7 @@ void editorProcessKeyPress() {
 
 
         case CTRL_KEY('s'):
-            editorsave();
+            editorSave();
             break;          
 
         case HOME_KEY:
